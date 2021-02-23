0026a8ce9bb31955a10feeefe4b1d136fe4f9f1b
WellyX4869 <wellysusanto4869@gmail.com>
2021-02-23 16:37:41 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  a = 0;
+  b = 0;
+
+  item = [
+    { nama: "Jeruk", harga: 15000, satuan: "kg" },
+    { nama: "Apel", harga: 5000, satuan: "kg" }
+  ];
+
+  tampil: boolean = false;
+  tulisanOPEN: String;
+
+  OPEN() {
+    this.tulisanOPEN = "TOKO DIBUKA";
+    alert("DIBUKA");
+  }
+
+  values = "";
+  onKey(event: any) {
+    this.values += event.target.value + '|';
+  }
 }
